a42cf651c4861feb102037dd559786f16c4da29c
Sayantan <sayantandatta97@gmail.com>
2018-05-14 01:42:30 +0530

    updated the color and tables.

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
 // Select color input
 // Select size input
-
+var height,width, color;
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(event){
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWeight').val();
+    makeGrid(height , width);
+    console.log("height "+height + " width "+ width);
+});
+function makeGrid(x,y) {
+        $('tr').remove();
+        for(var row=1; row<= x;row++){
+          $('#pixelCanvas').append('<tr id=table' + row +'></tr>');
+          for(var col=1;col<= y;col++){
+            $('#table' + row).append("<td></td>");
+          }
+        }
+     // adding color
+      $('td').click(function(){
+          color = $('#colorPicker').val();
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+          if($(this).attr("style")){
+             $(this).removeAttr('style');
+          }
+          else{
+            $(this).attr('style' , "background-color : "+ color);
+          }
+      });
+    }
